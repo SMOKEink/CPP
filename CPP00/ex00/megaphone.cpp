25a6ce0d681f0d5b05c7b1390a191ee6cf7b5fb5
Makevali <aachata@student.1337.ma>
2026-01-14 21:53:56 +0100

    change to for loop

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -2,26 +2,17 @@
 
 int main(int ac, char **av)
 {
-	int	i;
-	int	j;
-
 	if (ac == 1)
 	{
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return 0;
     }
-	i = 1;
-	while (i < ac)
+	for(int i = 1; i < ac; i++)
 	{
-		j = 0;
-		while (av[i][j])
-		{
+		for(int j = 0; av[i][j]; j++)
 			std::cout << (char)std::toupper(av[i][j]);
-			j++;
-		}
 		if (i + 1 < ac)
 			std::cout << " ";
-		i++;
 	}
 	std::cout << std::endl;
 }
